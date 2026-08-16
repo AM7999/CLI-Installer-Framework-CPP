8d5d8bd920e68af8fe2ade075fa93d0109f77d99
Xenia Nash <xenianash0@gmail.com>
2026-08-16 21:05:14 +0000

    Update Arches.cpp

--- src/Arches.cpp
+++ src/Arches.cpp
@@ -2,4 +2,6 @@
 
 #include "Package.h"
 
-std::string to_string(const Architecture &architecture)
+std::string to_string(const Architecture &architecture) {
+    switch(architecture) {
+        case Architecture::X86: return "x86";
